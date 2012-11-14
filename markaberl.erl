11c65cf39ebe66bfe98b7714df47ad85ad1530fb
Evan Farrar <evanfarrar@gmail.com>
2012-11-14 01:14:29 -0500

    Passing specs.

--- markaberl.erl
+++ markaberl.erl
@@ -1,14 +1,16 @@
 -module(markaberl).
--export([test/0]).
+-export([build/1, test/0]).
 
 test() ->
   "<tag>innertext</tag>" = build({tag, innertext}),
   "<tag><anothertag>innertext</anothertag></tag>" = build({tag, { anothertag, innertext}}),
-  "<tag><anothertag>innertext</anothertag><sibling>neighbor tag</sibling></tag>" = build({tag, [{ anothertag, innertext}, {sibling, neighbor}]}),
+  "<tag><anothertag>innertext</anothertag><sibling>neighbor</sibling></tag>" = build({tag, [{ anothertag, innertext}, {sibling, neighbor}]}),
   success.
 
 build({Tag, Body}) when is_tuple(Body) ->
   build({Tag, list_to_atom(build(Body))});
+build({Tag, Body}) when is_list(Body) ->
+    build({Tag, list_to_atom(lists:flatmap(fun build/1, Body))});
 build({Tag, Body}) ->
   TagString = atom_to_list(Tag),
   "<"++TagString++">"++atom_to_list(Body)++"</"++TagString++">".
