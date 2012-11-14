284e2c2b489d083688c5b8e8e2998df1a5e0d6e9
Evan Farrar <evanfarrar@gmail.com>
2012-11-14 01:06:36 -0500

    The final test

--- markaberl.erl
+++ markaberl.erl
@@ -4,6 +4,7 @@
 test() ->
   "<tag>innertext</tag>" = build({tag, innertext}),
   "<tag><anothertag>innertext</anothertag></tag>" = build({tag, { anothertag, innertext}}),
+  "<tag><anothertag>innertext</anothertag><sibling>neighbor tag</sibling></tag>" = build({tag, [{ anothertag, innertext}, {sibling, neighbor}]}),
   success.
 
 build({Tag, Body}) when is_tuple(Body) ->
